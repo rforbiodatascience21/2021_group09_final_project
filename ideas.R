527b588ee592540b38da93c11e057a9df2b889ff
angelams98 <angelams98@gmail.com>
2021-04-21 17:24:09 +0000

    This is a file with ideas for the project

---
+++ ideas.R
@@ -0,0 +1,13 @@
+#Ideas for the project
+#
+#We can merge some tables by the sample or by the OTU
+#We can see which ones are the most 
+#and less prevalent domains, phylum, class...
+#In each sample which is the most abundant microorganism
+# and the most affected parameters? Are these parameters
+#related to these micrroganisms always?
+#Which is the country with most variety of micrroganisms?
+#Is COD related to an especific factor? In NH4 related to pH?
+#Statistical analysis of the samples (linear regression, p-values...)
+#We can pivot a table according to the name of the samples 
+#(indicates the country)
