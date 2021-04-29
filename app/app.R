1797d682e76f92d6f286a7d42674f8786d37f154
angelams98 <angelams98@gmail.com>
2021-04-29 14:29:58 +0000

    ui changes

--- app/app.R
+++ app/app.R
@@ -4,6 +4,11 @@ library(shiny)
 library(shinythemes)
 
 # Define UI for application that draws a histogram
+dataset<-("breast_cancer_dataset")
+preview_mode<-c("Preview", "summary")
+columns <- c("patient_id", "education_type","birth_date", "age", "weight", "thickness_tumor")
+
+
 ui <- fluidPage(
     theme = shinytheme("flatly"),
     navbarPage(
@@ -11,18 +16,21 @@ ui <- fluidPage(
         tabPanel("Breast cancer data",
                  sidebarPanel(
                      tags$h3("Modify your dataset"),
-                     selectInput("dataset","Select your dataset",dataset.list),
-                     radioButtons("display_mode","Display",preview_mode.list),
-                     selectInput("columns","Select the columns you want to view or modify", columns.list, multiple = TRUE),
+                     selectInput("dataset","Select your dataset",dataset),
+                     radioButtons("display_mode","Display",preview_mode),
+                     selectInput("columns","Select the columns you want to view or modify",columns, multiple = TRUE),
                      h4("Filter"),
                      sliderInput("age", "Age", value = c(10, 20), min = 0, max = 100),
                      textInput("patient_id","Write the patients id"),
-                     radioButtons("gender", "Gender", c("male", "female"))
-                     
+                     radioButtons("gender", "Gender", c("male", "female")),
+                     actionButton("reset", "Reset")
                  ),
                  mainPanel(
-                     verbatimTextOutput("table")
-                 ))#end of the tabpanel
+                     dataTableOutput("Table"),
+                     
+                 )),#end of the tabpanel
+        tabPanel("Data information",
+                 verbatimTextOutput("summary"))#end of the tabpanel
         
     ) # navbarPage end
 )
