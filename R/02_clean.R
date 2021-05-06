9b72326606db19d8343f7f93de750b164f168207
HenrikSpiegel <henrik.spiegelhauer@gmail.com>
2021-05-06 17:12:38 +0200

    moved weight/age cleaning to 02clean.R

--- R/02_clean.R
+++ R/02_clean.R
@@ -54,8 +54,21 @@ comb_clean_data <- comb_data %>%
                      yes=blood, no=NA_real_),
       birth_date = ifelse(nchar(birth_date)==4,
                           yes = birth_date, no = NA_real_)
-    )
+    ) %>%
 
+  # low age and low weight
+  filter(age >= 20, weight >= 35) %>%
+  # not got period yet, but gave birth (= does not make sense) (1 person)
+  filter(!(menstrual_age == "not yet" & pregnency_experience == 0))
+
+# Remove inconsistencies in the data
+# Based on UK statistics, only look at women above 20 years old
+# (https://www.cancerresearchuk.org/health-professional/cancer-statistics/statistics-by-cancer-type/breast-cancer/incidence-invasive#heading-One)
+# Adult women 1-percentile is 145cm or lower
+# (https://dqydj.com/height-percentile-calculator-for-men-and-women/)
+# Calculating BMI, a 145cm woman is underweight if weight = 35 kg, extremely if under.
+# (https://getcalc.com/health-bmi-54kg-145cm.htm)
+# Hence remove all with weight under 35kg, as that is unlikely (also given how few are 145cm)
 
 # Write data --------------------------------------------------------------
 
