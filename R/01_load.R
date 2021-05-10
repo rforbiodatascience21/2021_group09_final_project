27769ac488a5fac929ba6dc01954c2da0a5cf0dd
idafrichhansen <s174601@student.dtu.dk>
2021-05-10 10:55:23 +0000

    tidy script

--- R/01_load.R
+++ R/01_load.R
@@ -9,24 +9,32 @@ library(readxl)
 
 
 # Load data ---------------------------------------------------------------
-
+# Temporary directory
 temp_dir = "data/temp"
-
 dir.create(temp_dir)
 
-
+# Unzip files
 path_zipped = "data/_raw/breastcancer_kaggle.zip"
+unzip(path_zipped, 
+      exdir = temp_dir)
 
-unzip(path_zipped, exdir = temp_dir)
-
+# Rename data names
 files <- list.files(path = temp_dir)
-
-data_names <- map_chr(files, ~str_remove (string = .x, ".xlsx")) %>%
-                    map_chr(~str_replace (.x, " ", "_"))
-
-data_dfs <- map(str_c(temp_dir, files, sep = "/"), ~ read_excel(path = .x, na = "-"))
+data_names <- map_chr(files, 
+                      ~str_remove (string = .x, 
+                                   ".xlsx")) %>%
+  map_chr(~str_replace (.x, 
+                        " ", 
+                        "_"))
+# Read excel files
+data_dfs <- map(str_c(temp_dir, 
+                      files, 
+                      sep = "/"), 
+                ~ read_excel(path = .x, 
+                             na = "-"))
 
 unlink(temp_dir, recursive = TRUE) 
+
 # Write data --------------------------------------------------------------
 
 file_names = str_c("01_", data_names, "_cases", ".csv", sep="")
