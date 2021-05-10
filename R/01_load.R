858ccb8bad31d313c74075dbc08554fd478a737e
angelams98 <angelams98@gmail.com>
2021-05-10 15:31:27 +0000

    spaces

--- R/01_load.R
+++ R/01_load.R
@@ -34,8 +34,8 @@ unlink(temp_dir, recursive = TRUE)
 
 # Write data --------------------------------------------------------------
 
-file_names = str_c("01_", data_names, "_cases", ".csv", sep="")
-file_paths = str_c("data", file_names, sep="/")
+file_names = str_c("01_", data_names, "_cases", ".csv", sep = "")
+file_paths = str_c("data", file_names, sep = "/")
 
 map2(data_dfs, file_paths, ~write_csv(x = .x, file = .y))
 
